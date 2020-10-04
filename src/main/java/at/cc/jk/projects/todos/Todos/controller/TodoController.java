dfebfa32b14ef7144f670ec82a9d769fbcdf9087
jkueng83 <johannes.kueng@gmx.at>
2020-10-04 07:12:57 +0200

    add: get todo by id

--- src/main/java/at/cc/jk/projects/todos/Todos/controller/TodoController.java
+++ src/main/java/at/cc/jk/projects/todos/Todos/controller/TodoController.java
@@ -2,9 +2,12 @@ package at.cc.jk.projects.todos.Todos.controller;
 
 import at.cc.jk.projects.todos.Todos.model.Todo;
 import at.cc.jk.projects.todos.Todos.repositories.TodoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,15 +16,21 @@ import java.util.List;
 @RestController
 public class TodoController {
 
+    private final Logger log = LoggerFactory.getLogger(TodoController.class);
+
     @Autowired
-    private TodoRepository repo;
+    private TodoRepository todoRepository;
 
     @GetMapping("/todos")
     List<Todo> getAllTodos() {
-
-        List<Todo> todos = repo.findAll();
-
+        List<Todo> todos = todoRepository.findAll();
         return todos;
+    }
+
+    @GetMapping("/todos/{id}")
+    Todo getTodo(@PathVariable("id") Integer id) {
+        log.info("get todo by id: {} ", id);
+        return todoRepository.getOne(id);
     }
 
 }
